f3610236b45d4f4fd11fb56ec239200483f02c9a
Arthur Pereira <arthurleandro50@gmail.com>
2025-04-11 10:34:49 -0300

    commit

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
+import Logo from './assets/logo_jac.png';
+// Pegando a largura da tela
+const larguraDaTela = Dimensions.get('window').width;
+const AlturaDaTela = Dimensions.get('window').height / 1.3;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={[styles.DivHome, { width: larguraDaTela, height: AlturaDaTela }]}>
+        <Image source={Logo} style={styles.imagem} />
+
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +22,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  DivHome: {
+    backgroundColor: '#36539B',
+  },
+  imagem: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
   },
 });
